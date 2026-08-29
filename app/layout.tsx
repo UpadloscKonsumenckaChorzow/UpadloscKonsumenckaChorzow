840f9985a01c06c4c58e0472e23377d0881188f3
FilipWrona06 <f.w9@interia.pl>
2026-08-29 21:39:18 +0200

    dodanie większej ikony

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,11 @@ export const metadata: Metadata = {
       { url: "/favicon.ico" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
     ],
     apple: "/apple-icon.png",
   },
